863d3f96188a2acd2b468d4b8367df8767696370
Frederick Kuhl <fkuhl@tyndalesoft.com>
2018-01-27 13:24:31 -0500

    Layout of Album collection is complete, except for the 3 Beatles albums with the big artwork!

--- ClassicPlayer/AlbumsViewController.swift
+++ ClassicPlayer/AlbumsViewController.swift
@@ -10,9 +10,11 @@ import UIKit
 import CoreData
 
 class AlbumCell: UICollectionViewCell {
+    @IBOutlet weak var stack: UIStackView!
     @IBOutlet weak var artwork: UIImageView!
     @IBOutlet weak var albumTitle: UILabel!
     @IBOutlet weak var artist: UILabel!
+    var stackWidthConstraint: NSLayoutConstraint?
 }
 
 fileprivate let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
@@ -29,6 +31,8 @@ class AlbumsViewController: UIViewController, NSFetchedResultsControllerDelegate
         super.viewDidLoad()
         self.collectionView.delegate = self //includes layout!
         self.collectionView.dataSource = self
+        (self.collectionView.collectionViewLayout as? UICollectionViewFlowLayout)?.estimatedItemSize
+            = UICollectionViewFlowLayoutAutomaticSize
         NotificationCenter.default.addObserver(self,
                                                selector: #selector(fontSizeChanged),
                                                name: .UIContentSizeCategoryDidChange,
@@ -53,12 +57,20 @@ class AlbumsViewController: UIViewController, NSFetchedResultsControllerDelegate
         request.sortDescriptors = [ NSSortDescriptor(key: "title", ascending: true) ]
         do {
             albums = try context.fetch(request)
+            setInterlineSpacing()
             collectionView.reloadData()
         }
         catch {
             let nserror = error as NSError
             fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
         }
+    }
+    
+    private func setInterlineSpacing() {
+        let subheadFont = UIFont.preferredFont(forTextStyle: .subheadline)
+        print("line height of \(subheadFont.lineHeight) for font \(subheadFont)")
+        (collectionView?.collectionViewLayout as? UICollectionViewFlowLayout)?.minimumLineSpacing = subheadFont.lineHeight * 2.0
+        collectionView?.collectionViewLayout.invalidateLayout()
     }
 
     override func didReceiveMemoryWarning() {
@@ -69,6 +81,7 @@ class AlbumsViewController: UIViewController, NSFetchedResultsControllerDelegate
     @objc
     func fontSizeChanged() {
         print("font size changed")
+        setInterlineSpacing()
         collectionView?.collectionViewLayout.invalidateLayout()
     }
 
@@ -83,6 +96,8 @@ class AlbumsViewController: UIViewController, NSFetchedResultsControllerDelegate
         return albums?.count ?? 0
     }
     
+    fileprivate let stackWidthIdentifier = "com.tyndalesoft.ClassicPlayer.stackWidth"
+    
     func collectionView(_ collectionView: UICollectionView,
                         cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Album",
@@ -95,6 +110,11 @@ class AlbumsViewController: UIViewController, NSFetchedResultsControllerDelegate
             let returnedArtwork = AppDelegate.artworkFor(album: realID)
             if returnedArtwork != nil {
                 cell.artwork.image = returnedArtwork
+//                let returnedFrame = cell.artwork.frame
+//                if returnedFrame.height > 150 {
+//                    //cell.artwork.frame = CGRect(origin: returnedFrame.origin, size: CGSize(width: returnedFrame.size.width, height: 150.0))
+//                    print("title: \(albumEntry.title) oversized: \(returnedFrame.size)")
+//                }
                 cell.artwork.isOpaque = true
                 cell.artwork.alpha = 1.0
             } else {
@@ -104,60 +124,74 @@ class AlbumsViewController: UIViewController, NSFetchedResultsControllerDelegate
                 cell.artwork.alpha = 0.3
             }
         }
+        cell.contentView.translatesAutoresizingMaskIntoConstraints = false
+        for constraint in cell.stack.constraints {
+            if constraint.identifier == stackWidthIdentifier { cell.stack.removeConstraint(constraint) }
+        }
+        let widthConstraint = cell.stack.widthAnchor.constraint(equalToConstant: cellWidthForTextSize())
+        widthConstraint.identifier = stackWidthIdentifier
+        widthConstraint.isActive = true
         return cell
+    }
+    
+    private func cellWidthForTextSize() -> CGFloat {
+        let subheadFont = UIFont.preferredFont(forTextStyle: .subheadline)
+        return max(150.0, 10.0 * subheadFont.pointSize) //10 is a magic number!
     }
 
     // MARK: - UICollectionViewDelegateFlowLayout
 
-    func collectionView(_ collectionView: UICollectionView,
-                        layout collectionViewLayout: UICollectionViewLayout,
-                        sizeForItemAt indexPath: IndexPath) -> CGSize {
-        print("layout item \(indexPath.row) font size \(UIApplication.shared.preferredContentSizeCategory)")
-        let artHeight = 150
-        var stackHeight = 0, stackWidth = 0
-        switch UIApplication.shared.preferredContentSizeCategory {
-        case .extraLarge:
-            stackWidth = 150
-            stackHeight = artHeight + 22 + 22 + 19
-        case .extraExtraLarge:
-            stackWidth = 170
-            stackHeight = artHeight + 24 + 24 + 21
-        case .extraExtraExtraLarge:
-            stackWidth = 190
-            stackHeight = artHeight + 26 + 26 + 23
-        case .accessibilityMedium:
-            stackWidth = 210
-            stackHeight = artHeight + 31 + 31 + 28
-        case .accessibilityLarge:
-            stackWidth = 230
-            stackHeight = artHeight + 37 + 37 + 32
-        case .accessibilityExtraLarge:
-            stackWidth = 250
-            stackHeight = artHeight + 43 + 43 + 39
-        case .accessibilityExtraExtraLarge:
-            stackWidth = 270
-            stackHeight = artHeight + 50 + 50 + 44
-        case .accessibilityExtraExtraExtraLarge:
-            stackWidth = 290
-            stackHeight = artHeight + 58 + 58 + 51
-        default:
-            stackWidth = 150
-            stackHeight = artHeight + 20 + 20 + 16
-        }
-        return CGSize(width: stackWidth, height: stackHeight)
-    }
-    
-    func collectionView(_ collectionView: UICollectionView,
-                        layout collectionViewLayout: UICollectionViewLayout,
-                        insetForSectionAt section: Int) -> UIEdgeInsets {
-        return sectionInsets
-    }
-    
-    func collectionView(_ collectionView: UICollectionView,
-                        layout collectionViewLayout: UICollectionViewLayout,
-                        minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return sectionInsets.left
-    }
+//    func collectionView(_ collectionView: UICollectionView,
+//                        layout collectionViewLayout: UICollectionViewLayout,
+//                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+//        print("layout item \(indexPath.row) font size \(UIApplication.shared.preferredContentSizeCategory)")
+//        let artHeight = 150
+//        var stackHeight = 0, stackWidth = 0
+//        switch UIApplication.shared.preferredContentSizeCategory {
+//        case .extraLarge:
+//            stackWidth = 150
+//            stackHeight = artHeight + 22 + 22 + 19
+//        case .extraExtraLarge:
+//            stackWidth = 170
+//            stackHeight = artHeight + 24 + 24 + 21
+//        case .extraExtraExtraLarge:
+//            stackWidth = 190
+//            stackHeight = artHeight + 26 + 26 + 23
+//        case .accessibilityMedium:
+//            stackWidth = 210
+//            stackHeight = artHeight + 31 + 31 + 28
+//        case .accessibilityLarge:
+//            stackWidth = 230
+//            stackHeight = artHeight + 37 + 37 + 32
+//        case .accessibilityExtraLarge:
+//            stackWidth = 250
+//            stackHeight = artHeight + 43 + 43 + 39
+//        case .accessibilityExtraExtraLarge:
+//            stackWidth = 270
+//            stackHeight = artHeight + 50 + 50 + 44
+//        case .accessibilityExtraExtraExtraLarge:
+//            stackWidth = 290
+//            stackHeight = artHeight + 58 + 58 + 51
+//        default:
+//            stackWidth = 150
+//            stackHeight = artHeight + 20 + 20 + 16
+//        }
+//        return CGSize(width: stackWidth, height: stackHeight)
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView,
+//                        layout collectionViewLayout: UICollectionViewLayout,
+//                        insetForSectionAt section: Int) -> UIEdgeInsets {
+//        return sectionInsets
+//    }
+
+//    func collectionView(_ collectionView: UICollectionView,
+//                        layout collectionViewLayout: UICollectionViewLayout,
+//                        minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+//        let subheadFont = UIFont.preferredFont(forTextStyle: .subheadline)
+//        print("line height of \(subheadFont.lineHeight) for font \(subheadFont)")
+//        return subheadFont.lineHeight * 2.0
+//    }
 
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
